002ec32db135b0e01a97b17a9a8e50acd86e2285
7sne <kooblatum@gmail.com>
2023-05-09 00:03:46 +0200

    Improve `242` solution

--- src/solutions/s242.rs
+++ src/solutions/s242.rs
@@ -1,27 +1,18 @@
 pub struct S242;
 
-fn chars_to_hm(s: String) -> std::collections::HashMap<char, i32> {
-    let chs = s.chars();
-    let mut hm: std::collections::HashMap<char, i32> =
-        std::collections::HashMap::with_capacity(chs.clone().count());
-    chs.for_each(|ch| {
-        hm.entry(ch).and_modify(|v| *v += 1).or_insert(1);
-    });
-    hm
-}
-
 impl S242 {
     pub fn is_anagram(s: String, t: String) -> bool {
         if s.len() != t.len() {
             return false;
         }
-        let mut hm = chars_to_hm(s);
-        for ch in t.chars() {
-            hm.entry(ch).and_modify(|v| {
-                *v -= 1;
-            });
-        }
-        hm.into_values().all(|v| v == 0)
+        let mut hm: std::collections::HashMap<u8, i32> = std::collections::HashMap::with_capacity(s.chars().count());
+        s.bytes().for_each(|ch| {
+            hm.entry(ch).and_modify(|v| *v += 1).or_insert(1);
+        });
+        t.bytes().for_each(|ch| {
+            hm.entry(ch).and_modify(|v| *v -= 1);
+        });
+        hm.values().all(|v| *v == 0)
     }
 }
 
